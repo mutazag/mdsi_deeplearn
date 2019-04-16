17d576f0fd63bd2eafb906b2f46eae026e9a8410
Mutaz Abu Ghazaleh <mutaz.m.abughazaleh@student.uts.edu.au>
2019-04-16 10:38:20 -0700

    playing with hyperparams for basic NN

--- at1/nn01.py
+++ at1/nn01.py
@@ -271,8 +271,8 @@ m = X.shape[1]
 n_x = 2     #No. of neurons in first layer
 n_h = 2     #No. of neurons in hidden layer
 n_y = 1     #No. of neurons in output layer
-num_of_iters = 10000
-learning_rate = 0.3
+num_of_iters = 1000
+learning_rate = .0001
 
 
 
